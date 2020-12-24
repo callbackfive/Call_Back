9b9f06958e05b078e6462be293393d790155b64f
winnie <asgh03226@gmail.com>
2020-12-24 20:15:51 +0800

    setting tailwindcss and rename folde javascript=>frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_12_22_180734) do
 
   # These are extensions that must be enabled in order to support this database
@@ -34,17 +33,12 @@ ActiveRecord::Schema.define(version: 2020_12_22_180734) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "fb_uid"
-    t.string "fb_token"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.datetime "birthday"
+    t.string "fb_uid"
+    t.string "fb_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["fb_uid"], name: "index_users_on_fb_uid"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
+    
+    require('tailwindcss')('./app/frontend/styles/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -9,4 +11,21 @@ module.exports = {
       stage: 3
     })
   ]
+};
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
+
+module.exports = environment;
