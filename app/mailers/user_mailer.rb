ec5bfef973e695c6ecd3158a7875d5154f7ba8d0
Phil Lee <prodigy7748g@gmail.com>
2020-12-24 17:50:20 +0800

    tmp

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
-  def notify_comment(user, comment)
-    @greeting = "Hi"	    
-    @comment = comment	    
+  def notify_comment(user)
+    # @greeting = "Hi"	    
+    # # @comment = comment	    
     mail(:to => user.email, :subject => "恭喜您註冊成功！")	    
-    # mail to: "to@example.org"	   
-  end	  
+    # # mail to: "to@example.org"	   
+  end	 
 end
+
+# UserMailer.new.notify_comment(User.first)
