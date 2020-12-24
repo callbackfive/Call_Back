2a3f906be69959e23fb5314a097a92c4ba7ce490
Phil Lee <prodigy7748g@gmail.com>
2020-12-24 15:33:32 +0800

    feat-add_edit_link_to_profile_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root to: "projects#index"
+  root to: "home#index"
 
   devise_for :users, controllers: { 
     sessions: 'users/sessions', 
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
+  resource :users, controller: 'profiles', only: [] do
+    get '/profile', action: 'show'
+  end
 
   resources :projects, except: [:destroy]
    
