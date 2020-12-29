39a10e5dfc4f879752644b890cba594855db4151
Phil Lee <prodigy7748g@gmail.com>
2020-12-29 12:55:39 +0800

    remove user sign in confirmation

--- db/migrate/20201227141153_update_column_to_giveback.rb
+++
@@ -1,6 +0,0 @@
-class UpdateColumnToGiveback < ActiveRecord::Migration[6.0]
-  def change
-    add_reference(:givebacks, :project, index: true)
-    remove_columns(:givebacks, :belongs_to, :project)
-  end
-end
--- db/migrate/20201227142626_add_ref_to_project.rb
+++
@@ -1,6 +0,0 @@
-class AddRefToProject < ActiveRecord::Migration[6.0]
-  def change
-    add_reference(:givebacks, :project, foreign_key: true)
-    remove_columns(:givebacks, :belongs_to, :project)
-  end
-end
