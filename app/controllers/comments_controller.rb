e925c6c8bb70bab620b5f38fc1fd64472c5fe9e5
Phil Lee <prodigy7748g@gmail.com>
2021-01-02 17:56:27 +0800

    done show comments in project show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
-class CommentController < ActionController::Base
+class CommentsController < ActionController::Base
+  def create
+    @project = Project.find(params[:project_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.project = @project
+     if @comment.save
+       redirect_to project_path(@project)
+     end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
