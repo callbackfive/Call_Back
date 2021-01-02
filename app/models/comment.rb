4298436fc9bf2f795edd6ee47dac1bd39ddafcd9
Phil Lee <prodigy7748g@gmail.com>
2021-01-02 21:36:03 +0800

    make a reply form partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to  :project
-  belongs_to  :parent, class_name: 'Comment', optional: true
-  has_many    :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :project
+  belongs_to :user
+  belongs_to :parent, class_name: 'Comment', optional: true
+  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
 
-  validates :body, presence: :true
+  validates :content, presence: :true
 end
