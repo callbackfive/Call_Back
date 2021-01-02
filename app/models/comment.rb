462b7b9382f2ca058b19d6e2446d55fa66db1cde
Phil Lee <prodigy7748g@gmail.com>
2021-01-02 21:16:11 +0800

    set self-joining association with comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :project
+  belongs_to  :project
+  belongs_to  :parent, class_name: 'Comment', optional: true
+  has_many    :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+
+  validates :body, presence: :true
 end
