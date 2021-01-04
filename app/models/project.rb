d3394e9b1fe13ca9aa9ef9f6ad7a0e6ae01f0b5b
Phil Lee <prodigy7748g@gmail.com>
2021-01-04 12:10:28 +0800

    解決留言回覆的排列順序(最新的回覆在下方)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :givebacks, :inverse_of => :project
-  has_many :comments
+  has_many :comments, -> { order('created_at DESC') }
   accepts_nested_attributes_for :givebacks, allow_destroy: true, reject_if: :all_blank
 end
