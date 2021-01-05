1a7bcc50b09da10a754b0352e9350f977d517c49
Phil Lee <prodigy7748g@gmail.com>
2021-01-05 11:59:54 +0800

    switch reply order to asc

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -24,3 +24,5 @@ const images = require.context('../images', true)
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
