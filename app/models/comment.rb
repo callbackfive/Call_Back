91dc5d3698b2164fc9b553c1c7a9bb0f351506d8
Phil Lee <prodigy7748g@gmail.com>
2021-01-05 11:59:54 +0800

    switch reply order to asc

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :project
   belongs_to :user
-  belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, optional: true
+  has_many :replies, -> { order('created_at ASC') }, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
 
   validates :content, presence: :true
-  
-  # default_scope { order('created_at DESC') }
 end
