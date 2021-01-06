28dedff93f939a7d4b77d0874e3545cf9ac9cd52
winnie <asgh03226@gmail.com>
2021-01-07 00:33:56 +0800

    - 增加Carousel - 增加募資項目的card
    待完成：
    首頁的RWD

---
+++ app/frontend/components/home/Banner.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import './Banner.scss'
+
+import BannerCard from './items'
+
+
+
+function BannerCarousel(){
+  return(
+    
+  <div className="banner-container">
+
+
+    
+  
+  <h1>Call Back your money!</h1>
+  <p>Let's get start!</p>
+  <div className="banner-btns flex">
+    <button className="signIn-btn" ></button>
+    <button className="" ></button>
+  </div> 
+    
+
+    </div>
+  );
+}
+
+export default BannerCarousel;
+
+
+
+
---
+++ app/frontend/components/home/items.js
@@ -0,0 +1,14 @@
+
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 75vh;
+  width: 100%;
+  background-color:＃E8EC67;
+  color: #FFF;
+  font-size: 4em;
+  outline:none;
+`;
