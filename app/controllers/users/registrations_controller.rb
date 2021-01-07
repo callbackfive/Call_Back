12f9f62e3fceae8c9c0d3bd59e7cbb9674c1be7d
Phil Lee <prodigy7748g@gmail.com>
2021-01-07 16:37:23 +0800

    add upload image section for user sign_up and update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,31 +2,31 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # # GET /resource/sign_up
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # # POST /resource
-  def create
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
-  def edit
-    super
-  end
+  # def edit
+  #   super
+  # end
 
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
 
   # # PUT /resource
-  def update
-    super
-  end
+  # def update
+  #   super
+  # end
 
   # # DELETE /resource
   # def destroy
@@ -50,9 +50,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
