7e1db96f2337bf96d5121bbd55a9240764f92d40
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-08 16:29:13 +0800

    add_order_show_page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :find_giveback, only: [:show, :edit, :update, :destroy]
+
+
+
+    private
+    def find_giveback
+        @giveback = Giveback.find(params[:id])
+    end
+
+
 end
