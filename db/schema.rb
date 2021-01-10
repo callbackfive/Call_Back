582d197c05279b0b7ecfc2074ac724f4e2d2578c
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-10 11:37:59 +0800

    add column 'status' to the table projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_010208) do
+ActiveRecord::Schema.define(version: 2021_01_10_025948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_010208) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
     t.datetime "deleted_at"
+    t.integer "status", default: 0
     t.index ["deleted_at"], name: "index_projects_on_deleted_at"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
