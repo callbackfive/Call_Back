53dbbdc35749f472d1b0814413cf5182abc83840
Phil Lee <prodigy7748g@gmail.com>
2021-01-11 23:11:30 +0800

    add categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,27 @@
 class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    @category.save
+    redirect_to categories_path
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+  
 end
