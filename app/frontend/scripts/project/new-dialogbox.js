3ea67d58b89c14ac96c58911418a00cc25949343
bobanderic2000 <bobanderic2000@gmail.com>
2021-01-11 19:09:16 +0800

    change coding style of new-dialogbox-popup.js

--- app/frontend/scripts/project/new-dialogbox.js
+++ app/frontend/scripts/project/new-dialogbox.js
@@ -3,14 +3,12 @@ document.addEventListener('turbolinks:load', () => {
   const popupBlock = document.querySelector('.new-dialogbox-popup-block');
   document.querySelector('.new-dialogbox-popup').addEventListener('click', (e) => {
     e.preventDefault();
-    popupBlock.classList.remove('no-popup');
-    popupBlock.classList.add('popuped');
+    popupBlock.style.display = "flex";
   });
 
   const closeIcon = document.querySelector('.close-popup');
   closeIcon.addEventListener('click', (e) => {
-    popupBlock.classList.remove('popuped');
-    popupBlock.classList.add('no-popup');
+    popupBlock.style.display = "none";
   });
 
 });
