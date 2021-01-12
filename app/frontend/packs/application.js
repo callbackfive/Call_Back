e2bfd9a86f6f7db7771c113d6d24528b2714c0e5
Jocelyn Lee <biheyeblue@gmail.com>
2021-01-12 17:36:34 +0800

    add_paid_orders_amounts

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -16,7 +16,9 @@ import "pages"
 
 const images = require.context('../images', true)
  
-
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/models/giveback.rb
+++ app/models/giveback.rb
@@ -1,6 +1,7 @@
 class Giveback < ApplicationRecord
   belongs_to :project
   has_many :orders
+  has_many :paid_orders, -> { where(status: [:paid])}, class_name: 'Order'
 
   mount_uploader :image, ImageUploader
   acts_as_paranoid
