35a751112de48d5c2dc36038b52fe4012c61bbba
Phil Lee <prodigy7748g@gmail.com>
2021-01-12 15:33:33 +0800

    add image column to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,10 @@
 class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @categories = Category.all
+  end
+  
   def new
     @category = Category.new
   end
@@ -9,19 +15,32 @@ class CategoriesController < ApplicationController
     redirect_to categories_path
   end
 
-  def index
-    @categories = Category.all
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     redirect_to categories_path
   end
 
   private
   def category_params
-    params.require(:category).permit(:title)
+    params.require(:category).permit(:title, :image)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_155806) do
+ActiveRecord::Schema.define(version: 2021_01_12_061236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_155806) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
