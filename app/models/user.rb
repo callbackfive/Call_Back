fa8a0c01b46d381aaecc9ebdfbfa34c0073f66f4
Phil Lee <prodigy7748g@gmail.com>
2021-01-13 00:11:38 +0800

    set fav icon for project show

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :dialogboxes
   has_many :orders
-  has_many :my_favorites, through: :fav_projects, source: 'post'
+  has_many :fav_projects
+  has_many :my_fav_projects, through: :fav_projects, source: 'post'
   mount_uploader :image, ImageUploader
   acts_as_paranoid
   validates :name, presence: true
@@ -39,6 +40,10 @@ class User < ApplicationRecord
     end
 
     user
+  end
+
+  def favorite?(project)
+    my_favorites.include?(project)
   end
 
   def delete_access_token(auth)
