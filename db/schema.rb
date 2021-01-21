57f15f7e0945797bf5da0b6844fd8b91a3f3b1cc
bobanderic2000 <bobanderic2000@gmail.com>
2021-01-21 13:04:55 +0800

    before 0121 demo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_134054) do
+ActiveRecord::Schema.define(version: 2021_01_19_160308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,18 @@ ActiveRecord::Schema.define(version: 2021_01_15_134054) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["dialogbox_id"], name: "index_messages_on_dialogbox_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient_type_and_recipient_id"
   end
 
   create_table "orders", force: :cascade do |t|
