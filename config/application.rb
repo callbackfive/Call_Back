63e79c749c3c89596c7f00d1a3f4949ec5e36110
Phil Lee <prodigy7748g@gmail.com>
2021-01-25 23:31:43 +0800

    resolve application.scss conflict

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,15 @@ module Callback
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       html_tag
     }
+    <?xml version="1.0" encoding="UTF-8"?>
+    <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <CORSRule>
+        <AllowedOrigin>https://example.com</AllowedOrigin>
+        <AllowedMethod>PUT</AllowedMethod>
+        <AllowedMethod>POST</AllowedMethod>
+        <MaxAgeSeconds>3600</MaxAgeSeconds>
+        <AllowedHeader>*</AllowedHeader>
+    </CORSRule>
+    </CORSConfiguration>
   end
 end
