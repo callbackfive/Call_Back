4cb986d0ff2bc259d4621472b6775d5146e29c46
Phil Lee <prodigy7748g@gmail.com>
2021-02-01 15:13:24 +0800

    refine routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  get '/apis/projects', action: 'projects',controller: 'apis'
-  get '/apis/categories', action: 'categories',controller: 'apis'
-  get '/apis/succeeded', action: 'succeeded',controller: 'apis'
-  
-  
   namespace :admin do
     root to: "users#index"
     resources :categories
@@ -19,53 +14,41 @@ Rails.application.routes.draw do
     resources :messages
   end
   
+  # --------------user-------------
   devise_for :users, controllers: { 
     sessions: 'users/sessions', 
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks",
   }
 
-  resource :users, controller: 'profiles', only: [] do
-    get '/profile', action: 'show'
-  end
-
-  resources :notifications, only: [:index]
-
   resources :users do
     resources :projects, only: [:index], action: :user_projects_index
     resources :orders, only: [:index, :new, :create, :show]
+    collection do
+      get '/profile' ,to: 'profiles#show'
+    end
   end
 
-  # projects
+  # --------------project-------------
   resources :projects, path_names: {new: 'proposal'} do
     member do
-      get 'myorderlist', as: 'myorderlist',action: :project_orders_index 
+      get 'myorderlist', action: :project_orders_index 
+      get 'rewards', action: :project_givebacks
+      post 'favorite'
     end
-    member do
-      get 'rewards', as: 'rewards', action: :project_givebacks
-    end
-    member do
-      post :favorite
-    end
+
     collection do
       get :my_favorite
     end
+
     resources :comments, shallow: true, only: [:new, :create, :destroy]
   end 
 
   resource :project, only: [] do
     resources :givebacks, except: [:new] 
   end
-
-  resources :users do
-    resources :projects, only: [:index], action: :user_projects_index
-    resources :orders, only: [:index, :new, :create, :show] do
-    end
-  end
-
-
-  resources :categories, shallow: true
   
+  # --------------payment-------------
   resources :payments do
     collection do
       get :mpg
@@ -79,11 +62,20 @@ Rails.application.routes.draw do
   
   # 訊息
   resources :messages, only: :index
+
   # 專案頁面創訊息
   post '/project/create_message', action: 'create_message', controller: 'projects'
+
   # 對話框創訊息
   post '/dialogbox/create_message', action: 'create_message', controller: 'dialogboxes'
 
+  resources :categories, shallow: true
+  
+  resources :notifications, only: [:index]
+
+  get '/apis/projects', action: 'projects',controller: 'apis'
+  get '/apis/categories', action: 'categories',controller: 'apis'
+  get '/apis/succeeded', action: 'succeeded',controller: 'apis' 
 end
 
 
